e01e619767da856feaea40d3eecb86a4e21d8b3f
Ranel Tagirov <raneltagirov06@mail.ru>
2025-12-06 23:55:58 +0300

    Hook main into parser and stub save/read handling

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,46 +1,27 @@
+#include "args_parser.hpp"
 #include <iostream>
-#include <string>
-#include <vector>
 
-namespace {
-
-void printHelp(const std::string &programName) {
-    std::cout << "Usage:\n"
-              << "  " << programName << " [command] [options]\n\n"
-              << "Commands:\n"
-              << "  --save \"<text>\"   Store a text record on the server\n"
-              << "  --read            Show all stored records\n"
-              << "  --help            Display help information\n\n"
-              << "Examples:\n"
-              << "  " << programName << " --save \"buy groceries\"\n"
-              << "  " << programName << " --read\n"
-              << "  " << programName << " --help\n";
-}
-
-bool hasHelpFlag(const std::vector<std::string> &args) {
-    for (const auto &arg : args) {
-        if (arg == "--help") {
-            return true;
-        }
-    }
-    return false;
-}
-
-}
-
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     const std::string programName = (argc > 0 && argv[0] != nullptr) ? argv[0] : "./cli/build/stu";
 
-    std::vector<std::string> args;
-    for (int i = 1; i < argc; ++i) {
-        args.emplace_back(argv[i]);
-    }
+    ArgsParser parser;
+    const ParseResult parsed = parser.parse(argc, argv);
 
-    if (args.empty() || hasHelpFlag(args)) {
-        printHelp(programName);
+    if (!parsed.check()) {
+        std::cerr << parsed.errorMessage << "\n\n";
+        ArgsParser::printHelp(programName);
+        return 1;
+    }
+    if (parsed.command == Help) {
+        ArgsParser::printHelp(programName);
+        return 0;
+    } else if (parsed.command == Save) {
+        std::cout << "Save command received. Text: " << parsed.text << "\n";
+        return 0;
+    } else if (parsed.command == Read) {
+        std::cout << "Read command received\n";
         return 0;
     }
 
-    std::cerr << "Unknown command. Use --help to see available commands\n";
     return 1;
 }
