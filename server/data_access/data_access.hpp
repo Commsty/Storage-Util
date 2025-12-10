4470dfac4ea818e9b1aa9f400a0b9f82c0d6cccf
Eva-Evans <68923254+Eva-Evans@users.noreply.github.com>
2025-12-11 02:31:19 +0300

    м1
    - Сохранение записей в JSON-файлы
    - Отдельный файл для каждого пользователя вроде бы
    - Автоматическое создание папок
    - Обработка ошибок

--- server/data_access/data_access.hpp
+++ server/data_access/data_access.hpp
@@ -5,11 +5,11 @@
 
 class DataAccessLayer {
 public:
-    ~DataAccessLayer() = default;
+    virtual ~DataAccessLayer() = default;
 
     // save сохраняет запись record для пользователя user_key
-    bool save(const std::string& user_key, const Record& record);
+    virtual bool save(const std::string& user_key, const Record& record);
 
     // load возвращает все записи пользователя user_key
-    std::vector<Record> load(const std::string& user_key);
+    virtual std::vector<Record> load(const std::string& user_key);
 };
